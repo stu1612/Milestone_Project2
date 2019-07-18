cb4694fc54e442b7618b3cba36d7af6f8e715712
Ubuntu <ubuntu@ip-172-31-30-27.us-east-2.compute.internal>
2019-07-18 23:09:46 +0000

    test id toggle navigation of two music genres working correctly - ready to add more music genres and incorporate the lon-lat coordinates in gmaps

---
+++ assets/JS/festivalData.js
@@ -0,0 +1,52 @@
+var data = [
+ 
+    //Rock section 
+     
+    
+    { 
+        "Event":"Rock in Rio Festival", 
+        "URL":"http://rockinrio.com/rio/en", 
+        "Image":"https://cdn.escapismmagazine.com/gallery/5379cfd5e0ad0.jpeg",
+        "Latitude":"39.564",
+        "Longitude":"-87.23724",
+        "Place":"Rio de Janeiro",
+        "Country":"Brazil",
+        "Genre":"Rock"
+    },
+    
+    { 
+        "Event":"Sziget festival", 
+        "URL":"https://szigetfestival.com/en/", 
+        "Image":"http://djambore.com/wp-content/uploads/2019/02/sziget-cover.jpg",
+        "Latitude":"47.33469",
+        "Longitude":"19.05002",
+        "Place":"Budapest",
+        "Country":"Hungary",
+        "Genre":"Rock"
+    },
+    
+    
+    {
+        "Event":"Coachella Festival", 
+        "URL":"https://www.coachella.com/", 
+        "Image":"https://cdn1.thr.com/sites/default/files/imagecache/list_landscape_960x541/2019/04/gettyimages-945956642-h_2019.jpg",
+        "Latitude":"33.77659",
+        "Longitude":"-116.2894",
+        "Place":"Indio CA",
+        "Country":"USA",
+        "Genre":"pop"
+    },
+    
+     {
+        "Event":"Isle of Wight Festival", 
+        "URL":"https://isleofwightfestival.com/", 
+        "Image":"https://www.visitisleofwight.co.uk/imageresizer/?image=%2Fdbimgs%2Fiow-festival-header.jpg&action=PageHeaderSuper",
+        "Latitude":"37.00561",
+        "Longitude":"-76.66292",
+        "Place":"Isle of Wight",
+        "Country":"UK",
+        "Genre":"Pop"
+    }
+    
+];  
+    
--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,7 +1,54 @@
-var myData = JSON.parse("festivalData.json");
+var myData = data;
 
 
-alert(myData[0].Event);
+//function to unhide image after click event
+function showImage(){
+        document.getElementById('img_1').style.visibility='visible';
+        document.getElementById('img_2').style.visibility='visible';
+        document.getElementById('img_3').style.visibility='visible';
+        document.getElementById('img_4').style.visibility='visible';
+        
+    }
+
+
+
+// ROCK - first festival content col
+document.getElementById('button_rock').addEventListener('click', function() {
+  
+  document.getElementById("title_1").innerHTML = myData[0].Event;
+  document.getElementById("text_1").innerHTML = myData[0].Place + " " + "," + myData[0].Country;
+  document.getElementById("link_1").innerHTML = myData[0].URL;
+  document.getElementById("img_1").src = myData[0].Image;
+
+// ROCK - second festival content col
+
+  document.getElementById("title_2").innerHTML = myData[1].Event;
+  document.getElementById("text_2").innerHTML = myData[1].Place + " " + "," + myData[1].Country;
+  document.getElementById("link_2").innerHTML = myData[1].URL;
+  document.getElementById("img_2").src = myData[1].Image;
+});
+
+// POP - first festival content col
+document.getElementById('button_pop').addEventListener('click', function() {
+  
+  document.getElementById("title_1").innerHTML = myData[2].Event;
+  document.getElementById("text_1").innerHTML = myData[2].Place + " " + "," + myData[2].Country;
+  document.getElementById("link_1").innerHTML = myData[2].URL;
+  document.getElementById("img_1").src = myData[2].Image;
+
+// POP - second festival content col
+
+  document.getElementById("title_2").innerHTML = myData[3].Event;
+  document.getElementById("text_2").innerHTML = myData[3].Place + " " + "," + myData[3].Country;
+  document.getElementById("link_2").innerHTML = myData[3].URL;
+  document.getElementById("img_2").src = myData[3].Image;
+});
+
+
+
+
+
+
 
 
  
