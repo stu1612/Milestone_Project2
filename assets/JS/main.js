7221df2b029932ace1dabeed092e9e531e51a33b
Ubuntu <ubuntu@ip-172-31-30-27.us-east-2.compute.internal>
2019-07-18 09:14:13 +0000

    test push to github acc

---
+++ assets/JS/main.js
@@ -0,0 +1,48 @@
+var myData = JSON.parse("festivalData.json");
+
+
+alert(myData[0].Event);
+
+
+ 
+ 
+ 
+ 
+ // map section ------------------------------
+ 
+ /*function initMap() {
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 3,
+          center: {lat: -28.024, lng: 140.887}
+        });
+
+        // Create an array of alphabetical characters used to label the markers.
+        var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        // Add some markers to the map.
+        // Note: The code uses the JavaScript Array.prototype.map() method to
+        // create an array of markers based on a given "locations" array.
+        // The map() method here has nothing to do with the Google Maps API.
+        var markers = locations.map(function(location, i) {
+          return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+          });
+        });
+
+        // Add a marker clusterer to manage the markers.
+        var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+      }
+      var locations = [
+        {lat: -31.563910, lng: 147.154312},
+        {lat: -33.718234, lng: 150.363181},
+        {lat: -33.727111, lng: 150.371124},
+        {lat: -33.848588, lng: 151.209834},
+        {lat: -33.851702, lng: 151.216968}
+        
+      ];*/
+      
+// end of map section
+
